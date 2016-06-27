a310b52cb50e8ed0109319a0b2bea7f323ae7a6e
Indranil Sen <indranilsen010@gmail.com>
2016-06-27 11:47:31 -0400

    Dependency checking added

--- webapp.sh
+++ webapp.sh
@@ -9,4 +9,36 @@ version="1.0.0"
 
 # ========================================
 
+# Script location
+scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+# Checking for homebrew dependencies
+# --------------------
+# Dependencies for running the webapp are listed in the
+# arrays. If not found, the dependices are installed.
+# --------------------
+homebrewDependencies=("neil")
+
+install=()
+
+checkBrewDependencies() {
+	for pkg in ${homebrewDependencies[@]}
+	do
+		if [ $(which $pkg 2>/dev/null) ]; then
+			continue
+		else
+			echo $pkg not installed
+			install+=("$pkg")
+		fi
+	done
+}
+
+installBrewDependencies() {
+	for pkg in ${install[@]}
+	do
+		"$(brew install "$pkg")"
+	done
+}
+
+checkBrewDependencies
+installBrewDependencies
