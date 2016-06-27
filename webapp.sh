6a8c5aa3699361fc33836b86d64c9cb6ed70e6b0
Indranil Sen <indranilsen010@gmail.com>
2016-06-27 10:53:51 -0400

    Initial commit

---
+++ webapp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ========================================
+# Author: Indranil Sen
+# 
+# This script creates a boilerplate for an
+# AngularJS web app.
+
+version="1.0.0"
+
+# ========================================
+
+
